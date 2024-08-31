5b089f6b162e3d0a5aa2714b855cc39a60704e5a
Pixel50 <jagger5674@gmail.com>
2024-08-31 22:19:43 +0300

    first commit

---
+++ db/Db.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import ru.netology.entity.MyEntity;
+import ru.netology.service.MyService;
+import java.util.UUID;
+
+public class Db {
+
+    private DbSetting dbSetting;
+    private MyEntity myEntity;
+
+    public Db(DbSetting dbSetting) {
+        this.dbSetting = dbSetting;
+        myEntity = new MyEntity("first");
+        myEntity.setId(UUID.randomUUID());
+    }
+
+    public DbSetting getDbSetting() {
+        return dbSetting;
+    }
+
+    public MyEntity getMyEntity() {
+        return myEntity;
+    }
+
+    public void setMyEntity(MyEntity myEntity) {
+        this.myEntity = myEntity;
+    }
+}
